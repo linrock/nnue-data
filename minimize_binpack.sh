382e99b10c95531069e92c24017e7d090a09293d
Linmiao Xu <linmiao.xu@gmail.com>
2023-07-01 12:30:01 -0400

    Default to 1 thread, do nothing if already minimized

--- minimize_binpack.sh
+++ minimize_binpack.sh
@@ -5,12 +5,17 @@ if [ "$#" -ne 1 ]; then
 fi
 
 input_binpack=$1
+output_binpack=${input_binpack}.min.binpack
+if [ -f $output_binpack ]; then
+  echo Minimized binpack already exists: $output_binpack
+  exit 0
+fi
 
 options="
 uci
-setoption name Threads value 32
+setoption name Threads value 1
 isready
-transform minimize_binpack input_file ${input_binpack} output_file ${input_binpack}.min.binpack
+transform minimize_binpack input_file ${input_binpack} output_file ${ouput_binpack}
 quit"
 
 printf "$options" | stockfish-bin-min
