9386dd97a285a0ef468a77cdcf448f0b1cc63c55
Linmiao Xu <linmiao.xu@gmail.com>
2023-09-12 15:53:53 -0400

    Default to v6 filtering. Don't remove base binpacks

--- many_csv_to_filtered_binpacks.sh
+++ many_csv_to_filtered_binpacks.sh
@@ -7,19 +7,19 @@ fi
 function csv_zst_to_filtered_binpack() {
   set -eu -o pipefail
   input_csv_zst_filename=$1
-  filtered_plain_filename=${input_csv_zst_filename}.filter-v8.plain
-  output_filtered_binpack_filename=${input_csv_zst_filename}.filter-v8.binpack
-  filter_log_filename=${input_csv_zst_filename}.filter-v8.log
+  filtered_plain_filename=${input_csv_zst_filename}.filter-v6.plain
+  output_filtered_binpack_filename=${input_csv_zst_filename}.filter-v6.binpack
+  filter_log_filename=${input_csv_zst_filename}.filter-v6.log
   if [ -f $output_filtered_binpack_filename ]; then
     echo "Doing nothing, filtered binpack exists: $output_filtered_binpack_filename"
   else
-    echo "Filtering v8 ... $input_csv_zst_filename" | tee $filter_log_filename
-    python3 /home/ubuntu/csv_filter_v8.py $input_csv_zst_filename >> $filter_log_filename
+    echo "Filtering v6 ... $input_csv_zst_filename" | tee $filter_log_filename
+    python3 /home/ubuntu/csv_filter_v6.py $input_csv_zst_filename >> $filter_log_filename
     stockfish convert $filtered_plain_filename $output_filtered_binpack_filename >> $filter_log_filename
     rm $filtered_plain_filename
     ls -lth $output_filtered_binpack_filename >> $filter_log_filename
     minimize_binpack.sh $output_filtered_binpack_filename >> $filter_log_filename
-    rm $output_filtered_binpack_filename
+    # rm $output_filtered_binpack_filename
     ls -lth ${output_filtered_binpack_filename}.min.binpack >> $filter_log_filename
   fi
 }
